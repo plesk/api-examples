8207a8c9dfeccd7bf24df69e8149426949a0bf71
Alexei Yuzhakov <ayuzhakov@parallels.com>
2013-06-06 14:57:41 +0700

    Add example for Ruby.

---
+++ ruby/example.rb
@@ -0,0 +1,20 @@
+require 'plesk_api_client'
+
+host = ENV['REMOTE_HOST']
+login = ENV['REMOTE_LOGIN']
+password = ENV['REMOTE_PASSWORD']
+
+client = PleskApiClient.new(host)
+client.set_credentials(login, password)
+
+request = <<eof
+<packet version="1.6.3.0">
+  <server>
+    <get_protos/>
+  </server>
+</packet>
+eof
+
+response = client.request(request)
+puts response
+
---
+++ ruby/plesk_api_client.rb
@@ -0,0 +1,43 @@
+require 'net/http'
+require 'net/https'
+
+class PleskApiClient
+
+  def initialize(host, port = 8443, protocol = 'https')
+    @host = host
+    @port = port
+    @protocol = protocol
+  end
+
+  def set_credentials(login, password)
+    @login = login
+    @password = password
+  end
+
+  def set_secret_key(secret_key)
+    @secret_key = secret_key
+  end
+
+  def request(body)
+    http_request = Net::HTTP::Post.new('/enterprise/control/agent.php')
+    http_request.body = body
+    http_request.content_type = 'text/xml'
+    http_request.add_field 'HTTP_PRETTY_PRINT', 'TRUE'
+
+    if @secret_key
+      http_request.add_field 'KEY', @secret_key
+    else
+      http_request.add_field 'HTTP_AUTH_LOGIN', @login
+      http_request.add_field 'HTTP_AUTH_PASSWD', @password
+    end
+
+    http = Net::HTTP.new(@host, @port)
+    if 'https' == @protocol
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    response = http.start{ |http| http.request(http_request) }
+  end
+
+end
