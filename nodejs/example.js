65416a90c737c9fab060555cd0bbf3ce38c76930
Alexei Yuzhakov <sibprogrammer@gmail.com>
2014-10-10 09:36:24 +0700

    Allow to use self-signed SSL certificate on Plesk for node.js example.

--- nodejs/example.js
+++ nodejs/example.js
@@ -6,6 +6,8 @@ var host = process.env.REMOTE_HOST;
 var login = process.env.REMOTE_LOGIN;
 var password = process.env.REMOTE_PASSWORD;
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 var client = new pleskApi.Client(host);
 client.setCredentials(login, password);
 
