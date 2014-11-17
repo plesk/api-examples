3a48f74e2f3b3590d07693bc70b4d048fe743301
Alexander Voronkov <alxn1101r@gmail.com>
2014-11-17 19:32:19 +0700

    Add example for Go

---
+++ go/plesk_api_client.go
@@ -0,0 +1,75 @@
+// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
+package main
+
+import (
+	"net/http"
+	"crypto/tls"
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
+
+type PleskApiClient struct {
+	host string
+	port uint
+	protocol string
+	login string
+	password string
+	secretKey string
+	InsecureSkipVerify bool
+}
+
+func NewPleskApiClient(host string) *PleskApiClient {
+	c := new(PleskApiClient)
+	c.host = host
+	c.port = 8443
+	c.protocol = "https"
+	return c
+}
+
+func (c *PleskApiClient) SetPort(port uint) {
+	c.port = port
+}
+
+func (c *PleskApiClient) SetProtocol(protocol string) {
+	c.protocol = protocol
+}
+
+func (c *PleskApiClient) SetCredentials(login, password string) {
+	c.login, c.password = login, password
+}
+
+func (c *PleskApiClient) SetSecretKey(secret_key string) {
+	c.secretKey = secret_key
+}
+
+func (c *PleskApiClient) Request(request string) (response string, err error) {
+	url := fmt.Sprintf("%s://%s:%d/enterprise/control/agent.php", c.protocol, c.host, c.port)
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(request))
+	if err != nil { return }
+	req.Header.Add("Content-type", "text/xml")
+	req.Header.Add("HTTP_PRETTY_PRINT", "TRUE")
+	if c.secretKey != "" {
+		req.Header.Add("KEY", c.secretKey)
+	} else {
+		req.Header.Add("HTTP_AUTH_LOGIN", c.login)
+		req.Header.Add("HTTP_AUTH_PASSWD", c.password)
+	}
+
+	client := http.Client{}
+	if c.InsecureSkipVerify {
+		tr := &http.Transport {
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client.Transport = tr
+	}
+
+	resp, err := client.Do(req)
+	if err != nil { return }
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	response = string(bytes)
+	return
+}
+
