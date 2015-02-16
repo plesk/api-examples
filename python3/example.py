c6bdba1e8b3e8f2c5fdb7decab96370fbe1736a3
Alexei Yuzhakov <sibprogrammer@gmail.com>
2015-02-16 11:21:51 +0600

    Add example for Python 3.

---
+++ python3/example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
+
+import os
+from plesk_api_client import PleskApiClient
+
+host = os.getenv('REMOTE_HOST')
+login = os.getenv('REMOTE_LOGIN', 'admin')
+password = os.getenv('REMOTE_PASSWORD')
+
+client = PleskApiClient(host)
+client.set_credentials(login, password)
+
+request = """
+<packet>
+  <server>
+    <get_protos/>
+  </server>
+</packet>
+"""
+
+response = client.request(request)
+print(response)
---
+++ python3/plesk_api_client.py
@@ -0,0 +1,39 @@
+# Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
+
+import http.client
+
+class PleskApiClient:
+
+    def __init__(self, host, port = 8443, protocol = 'https'):
+        self.host = host
+        self.port = port
+        self.protocol = protocol
+        self.secret_key = None
+
+    def set_credentials(self, login, password):
+        self.login = login
+        self.password = password
+
+    def set_secret_key(self, secret_key):
+        self.secret_key = secret_key
+
+    def request(self, request):
+        headers = {}
+        headers["Content-type"] = "text/xml"
+        headers["HTTP_PRETTY_PRINT"] = "TRUE"
+
+        if self.secret_key:
+            headers["KEY"] = self.secret_key
+        else:
+            headers["HTTP_AUTH_LOGIN"] = self.login
+            headers["HTTP_AUTH_PASSWD"] = self.password
+
+        if 'https' == self.protocol:
+            conn = http.client.HTTPSConnection(self.host, self.port)
+        else:
+            conn = http.client.HTTPConnection(self.host, self.port)
+
+        conn.request("POST", "/enterprise/control/agent.php", request, headers)
+        response = conn.getresponse()
+        data = response.read()
+        return data.decode("utf-8")
